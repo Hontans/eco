a7f09b92810584cc97c23b10181598a3bb95d9cd
Hontans <hontanssylvain@gmail.com>
2025-05-14 10:42:18 +0200

    refactor: simplifier la structure du composant MainLayout et améliorer la barre de recherche

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,25 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
+        <q-avatar size="125px">
+          <img
+            src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
+          />
+        </q-avatar>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-input class="search-bar" rounded standout v-model="text" label="rechercher un produit">
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+          <template v-slot:append>
+            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+          </template>
+        </q-input>
+        <div class="q-gutter-sm">
+          <q-avatar color="black" text-color="pink" icon="person" />
+          <q-avatar color="black" text-color="pink" icon="shopping_cart" />
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -18,5 +30,13 @@
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue';
+const text = ref('');
 </script>
+
+<style>
+.search-bar {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>

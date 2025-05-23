8c2d868a7e77d183dcbaf7db4ee1923ae10312f3
Hontans <hontanssylvain@gmail.com>
2025-05-23 11:37:14 +0200

    feat: réorganiser la page produit avec un carrousel d'images et améliorer la structure du code

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,36 +1,40 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-card class="my-card">
-      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+  <div class="q-pa-md">
+    <q-carousel
+      swipeable
+      animated
+      v-model="slide"
+      thumbnails
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg" />
+      <q-carousel-slide :name="2" img-src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg" />
+      <q-carousel-slide :name="3" img-src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg" />
+      <q-carousel-slide :name="4" img-src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Begrippenlijst.svg" />
+    </q-carousel>
+  </div>
+  <div>
 
-      <q-card-section>
-        <div class="text-h6">{{ product?.name }}</div>
-        <div class="text-subtitle2">{{ product?.price }}</div>
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        {{ lorem }}
-      </q-card-section>
-    </q-card>
-  </q-layout>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted } from 'vue';
 import { dataStore } from '../stores/data-store';
 
-const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+const lorem =
+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const slide = ref(1)
 const store = dataStore();
 const props = defineProps({
-  id: Number
-})
+  id: Number,
+});
 const productId = ref(props.id);
-const product = ref()
+const product = ref();
 
-function loadProduct()
-{
+function loadProduct() {
   if (productId.value) {
-    product.value = store.getProductById((productId.value))
+    product.value = store.getProductById(productId.value);
   }
 }
 
@@ -38,18 +42,22 @@ watch(
   () => props.id,
   (newId) => {
     productId.value = newId;
-    loadProduct()
-  }
-)
+    loadProduct();
+  },
+);
 
-onMounted(()=> {
-  loadProduct()
-})
-
+onMounted(() => {
+  loadProduct();
+});
 </script>
 
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 250px
+<style lang="scss" scoped>
+.my-card{
+  width: 100%;
+  max-width: 250px;
+}
+
+.q-carousel__thumbnail  {
+  cursor: pointer;
+}
 </style>
